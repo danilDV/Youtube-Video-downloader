c71e0554f0ff8c0f3ccc974c16df5ceefae00398
danilDV <glazovojd@gmail.com>
2022-04-26 19:06:58 +0600

    Youtube Video downloader realease

---
+++ main.py
@@ -0,0 +1,23 @@
+from pytube import YouTube
+
+storage = r'C:\Users\glazo\Desktop\YouTube'
+url = input('Enter video URL: ')
+yt = YouTube(url)
+
+mp4_files = yt.streams.filter(file_extension='mp4')
+mp4_720p_files = mp4_files.get_by_resolution("720p")
+mp4_720p_files.download(storage)
+print(f"Downloaded succesfully into {storage}")
+
+a = input("Do you want to download another video? (Yes/No): ").lower()
+
+while a != 'no':
+    url = input('Enter video URL: ')
+    yt = YouTube(url)
+
+    mp4_files = yt.streams.filter(file_extension='mp4')
+    mp4_720p_files = mp4_files.get_by_resolution("720p")
+    mp4_720p_files.download(storage)
+    print("Downloaded succesfully")
+if a == 'no':
+    print('Program finished, thanks for using')
